5c644c0b1ee706988a80a2c3657ca942447c3fb3
Matteo <matpaolieri@gmail.com>
2020-12-12 15:29:17 +0100

    Update pyNormalizePlot.py

--- pyNormalizePlot.py
+++ pyNormalizePlot.py
@@ -15,7 +15,7 @@ License: MIT
 
 import os
 
-# get the only file ending with IR.dat
+# get the only file ending with .dat
 
 vs_files = [f for f in os.listdir('.') if f.endswith('IR.dat')]
 if len(vs_files) != 1:
@@ -49,12 +49,13 @@ for i in range(0, tot_numbers):
     wavenumber_list[i] = float(wavenumber_list[i])
     intensity_list[i] = float(intensity_list[i])
 
-max_abs = max(intensity_list)
+# normalize all abs data: z_i = (x_i-min)/(max-min)
 
-# divide by the maximum intensity and normalize
+max_abs = max(intensity_list)
+min_abs = min(intensity_list)
 
 for i in range(0, tot_numbers):
-    intensity_list[i] = intensity_list[i]/max_abs
+    intensity_list[i] = (intensity_list[i]-min_abs)/(max_abs-min_abs)
 
 # write a new file "IR-norm.dat"
 
