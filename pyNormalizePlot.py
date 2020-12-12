286a8cdb3c74002f013845cef2221e4d5a358ca0
Matteo <matpaolieri@gmail.com>
2020-12-12 17:17:35 +0100

    Added reverse option

--- pyNormalizePlot.py
+++ pyNormalizePlot.py
@@ -13,52 +13,66 @@ Author: Matteo Paolieri, University of Cologne 2020
 License: MIT
 """
 
-import os
+import argparse
 
-# get the only file ending with .dat
 
-vs_files = [f for f in os.listdir('.') if f.endswith('IR.dat')]
-if len(vs_files) != 1:
-    raise ValueError('There should be only one file ending with IR.dat in the current directory!')
+def main(inputf, outputf, reverse):
 
-filename = vs_files[0]
+    # read it
 
-# read it
+    with open(inputf, 'r') as f:
+        all_text = f.readlines()
 
-with open(filename, 'r') as f:
-    all_text = f.readlines()
+    wavenumber_list = []
+    intensity_list = []
 
-wavenumber_list = []
-intensity_list = []
+    # extract wavenumbers and intensities
 
-# extract wavenumbers and intensities
+    for line in all_text:
+        column = line.split()
+        wavenumber = column[0]
+        intensity = column[1]
 
-for line in all_text:
-    column = line.split()
-    wavenumber = column[0]
-    intensity = column[1]
+        wavenumber_list.append(wavenumber)
+        intensity_list.append(intensity)
 
-    wavenumber_list.append(wavenumber)
-    intensity_list.append(intensity)
+    tot_numbers = int(len(intensity_list))
 
-tot_numbers = int(len(intensity_list))
+    # put everything in float
 
-# put everything in float
-
-for i in range(0, tot_numbers):
-    wavenumber_list[i] = float(wavenumber_list[i])
-    intensity_list[i] = float(intensity_list[i])
-
-# normalize all abs data: z_i = (x_i-min)/(max-min)
-
-max_abs = max(intensity_list)
-min_abs = min(intensity_list)
-
-for i in range(0, tot_numbers):
-    intensity_list[i] = (intensity_list[i]-min_abs)/(max_abs-min_abs)
-
-# write a new file "IR-norm.dat"
-
-with open('IR-norm.dat', 'w') as f:
     for i in range(0, tot_numbers):
-        print(f'{wavenumber_list[i]} {intensity_list[i]}', file=f)
+        wavenumber_list[i] = float(wavenumber_list[i])
+        intensity_list[i] = float(intensity_list[i])
+
+    # normalize all abs data: z_i = (x_i-min)/(max-min)
+
+    max_abs = max(intensity_list)
+    min_abs = min(intensity_list)
+
+    if reverse is True:
+        for i in range(0, tot_numbers):
+            intensity_list[i] = (intensity_list[i]-max_abs)/(min_abs-max_abs)
+    else:
+        for i in range(0, tot_numbers):
+            intensity_list[i] = (intensity_list[i]-min_abs)/(max_abs-min_abs)
+
+    # write a new file
+
+    with open(outputf, 'w') as f:
+        for i in range(0, tot_numbers):
+            print(f'{wavenumber_list[i]} {intensity_list[i]}', file=f)
+        
+
+if __name__ == "__main__":
+    # parser for shell
+    parser = argparse.ArgumentParser(description='Normalize a plot.')
+    parser.add_argument('--inputfile', type=str, default="IR-norm.dat",
+                        help="Input file, like a .dat, .txt ... With two columns: "
+                             "Wavenumbers and Intensity. Default is 'IR.dat'")
+    parser.add_argument('--outputfile', type=str, default="IR-norm.dat",
+                        help="Output file, default is 'IR-norm.dat'")
+    parser.add_argument('--reversey', type=bool, default=False,
+                        help="Choose whether reverse the y-axis or not. Default False.")
+
+    args = parser.parse_args()
+    main(args.inputfile, args.outputfile, args.reversey)
